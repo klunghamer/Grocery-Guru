f137c2a95fa4b36340d7682f17ecede37971758e
Kelly Lunghamer <klung@umich.edu>
2016-11-08 19:01:18 -0500

    Trying to fix geolocation error

--- controllers/helpers.js
+++ controllers/helpers.js
@@ -5,14 +5,14 @@ var request = require('request');
 
 router.get('/stores/:lat/:long', function(req, res) {
   console.log('latitude>>', req.params.lat);
-  console.log('key>', process.env.GOOGLE_KEY);
-  console.log('longitude>>', req.params.long);
-  request(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.params.lat},${req.params.long}&radius=3000&type=grocery_or_supermarket&keyword=food&key=${process.env.GOOGLE_KEY}`, function (error, response, body) {
-    if(!error && response.statusCode == 200) {
-      var parse = JSON.parse(body);
-      res.json(parse);
-    }
-  })
+  // console.log('key>', process.env.GOOGLE_KEY);
+  // console.log('longitude>>', req.params.long);
+  // request(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${req.params.lat},${req.params.long}&radius=3000&type=grocery_or_supermarket&keyword=food&key=${process.env.GOOGLE_KEY}`, function (error, response, body) {
+  //   if(!error && response.statusCode == 200) {
+  //     var parse = JSON.parse(body);
+  //     res.json(parse);
+  //   }
+  // })
 })
 
 
